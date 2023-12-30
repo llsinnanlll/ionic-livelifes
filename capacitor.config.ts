d0df619fddb94909ee60b9fdceedf3950546dc2d
Jose Ferrús Aparicio <jose.ferrus.94@gmail.com>
2023-12-30 15:53:42 +0100

    Version fallida

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lifelives.app',
+  appName: 'LiveLifes',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
